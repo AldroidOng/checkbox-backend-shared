951d20374d343b51219d1e07d8a333444c2e453b
Alvin Ong <alvin.ong@instahome.com>
2024-10-12 02:34:01 +0800

    Update on seed date to create task

--- database-setup/seeders/20241011165104-user-and-task.js
+++ database-setup/seeders/20241011165104-user-and-task.js
@@ -10,62 +10,61 @@ module.exports = {
       [
         {
           email: 'user1@example.com',
-          password: 'password1', // Use a hashed password in production
+          password: 'password1',
           name: 'User One',
         },
         {
           email: 'user2@example.com',
-          password: 'password2', // Use a hashed password in production
+          password: 'password2',
           name: 'User Two',
         },
       ],
       { returning: true },
     );
 
-    // // Generate tasks for the users
-    // const userId1 = users[0].id; // Get the ID of the first user
-    // const userId2 = users[1].id; // Get the ID of the second user
+    // Generate tasks for the users
+    const userId1 = users[0].id;
 
-    // await queryInterface.bulkInsert('tasks', [
-    //   // 2 tasks due more than 7 days from now
-    //   {
-    //     userId: userId1,
-    //     name: 'Task 1',
-    //     description: 'This task is due more than 7 days from now.',
-    //     dueDate: new Date(today.setDate(today.getDate() + 10)), // 10 days from today
-    //   },
-    //   {
-    //     userId: userId1,
-    //     name: 'Task 2',
-    //     description: 'This task is due more than 7 days from now.',
-    //     dueDate: new Date(today.setDate(today.getDate() + 14)), // 14 days from today
-    //   },
-    //   // 2 tasks due less than 7 days but more than today
-    //   {
-    //     userId: userId1,
-    //     name: 'Task 3',
-    //     description: 'This task is due in 5 days.',
-    //     dueDate: new Date(today.setDate(today.getDate() + 5)), // 5 days from today
-    //   },
-    //   {
-    //     userId: userId1,
-    //     name: 'Task 4',
-    //     description: 'This task is due in 3 days.',
-    //     dueDate: new Date(today.setDate(today.getDate() + 3)), // 3 days from today
-    //   },
-    //   // 1 task that is past the due date
-    //   {
-    //     userId: userId1,
-    //     name: 'Task 5',
-    //     description: 'This task is overdue.',
-    //     dueDate: new Date(today.setDate(today.getDate() - 1)), // 1 day in the past
-    //   },
-    // ]);
+    await queryInterface.bulkInsert('tasks', [
+      // 2 tasks due more than 7 days from now
+      {
+        userId: userId1,
+        name: 'Task 1',
+        description: 'This task is due more than 7 days from now.',
+        dueDate: new Date(today.setDate(today.getDate() + 10)), // 10 days from today
+      },
+      {
+        userId: userId1,
+        name: 'Task 2',
+        description: 'This task is due more than 7 days from now.',
+        dueDate: new Date(today.setDate(today.getDate() + 14)), // 14 days from today
+      },
+      // 2 tasks due less than 7 days but more than today
+      {
+        userId: userId1,
+        name: 'Task 3',
+        description: 'This task is due in 5 days.',
+        dueDate: new Date(today.setDate(today.getDate() + 5)), // 5 days from today
+      },
+      {
+        userId: userId1,
+        name: 'Task 4',
+        description: 'This task is due in 3 days.',
+        dueDate: new Date(today.setDate(today.getDate() + 3)), // 3 days from today
+      },
+      // 1 task that is past the due date
+      {
+        userId: userId1,
+        name: 'Task 5',
+        description: 'This task is overdue.',
+        dueDate: new Date(today.setDate(today.getDate() - 1)), // 1 day in the past
+      },
+    ]);
   },
 
   async down(queryInterface, Sequelize) {
     // Remove tasks and users
-    // await queryInterface.bulkDelete('tasks', null, {});
+    await queryInterface.bulkDelete('tasks', null, {});
     await queryInterface.bulkDelete('users', null, {});
   },
 };
